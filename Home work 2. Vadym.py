39df782c2a2c7e807a506cf074e96772956a2999
Вадим Нагих <97698395+Nocks123@users.noreply.github.com>
2022-01-18 15:48:03 +0200

    Add files via upload

---
+++ Home work 2. Vadym.py
@@ -0,0 +1,31 @@
+#Exercise №1
+a=113
+b=10
+c=int(a)+b
+print(c)
+123
+
+#Exercise №2
+d=113
+e=10
+f=float(d)+e
+print(f)
+123.0
+
+#Exercise №3
+g=12.345
+h=(10)
+j=int(g)+12333
+print(j)
+
+
+#Exercise №4
+number_card = input("Enter sixteen digit number: ")
+if number_card == "1234567890123456":
+    print("Welcome")
+else:
+    print("Access is denied")
+#Exercise №5
+x= int(input("Enter your number:   "))
+y= int(input("Enter your number:   "))
+print(x+y)
