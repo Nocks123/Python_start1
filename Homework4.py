a149bb8ca4564aaa0b46f4e122a745e47c9491e4
Вадим Нагих <97698395+Nocks123@users.noreply.github.com>
2022-01-24 19:12:58 +0200

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,24 @@
+	#Exercise 1
+x=14//2+49//7+500%26+1**3*6*1294
+y=5587+1
+z=[[5,2,8,10,16,19,7],[18,y,7,11,25,91,13],[8,15,20,21,62,22,64,82],[24,36,x,45,95,48,87,97]]
+e=z[3][2]
+print(e)
+z.sort(reverse=True)
+f=z[1][2]
+print(y)
+
+
+	#Exercise 2
+x=int(input("Enter any number: "))
+a=list(str(x))
+b=a[:]
+b.reverse()
+print(a==b)
+
+
+	#Exercise 3
+r=4
+x=float(input("Enter point coordinates X:  "))
+y=float(input("Enter point coordinates Y:  "))
+print(x**2+y**2<=r**2)
