4fdf706a91fda09252a73271e0241635eefc3095
Вадим Нагих <97698395+Nocks123@users.noreply.github.com>
2022-02-10 18:46:54 +0200

    Add files via upload

---
+++ Homework 9.py
@@ -0,0 +1,12 @@
+﻿#Exercise 1
+a=input("Enter any word: ")
+b=input("Enter any word: ")
+a=set("#".join(a.split()))
+b=set("#".join(b.split()))
+print(a&b)
+
+#Exercise 2
+from random import randrange 
+a=set([randrange(3,50,3)for _ in range(10)])
+b=set([randrange(5,50,5)for _ in range(10)])
+print(a,b)
